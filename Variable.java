ec05e125f1c867def8c75c2a7e7fd099d7f1bf93
VinitM <154134270+VinitBM@users.noreply.github.com>
2024-03-01 23:01:58 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,22 @@
+package corejava.basics;
+public class Variable {
+
+
+		byte num1;
+		int num2;
+	//char ch;
+		boolean isCompleted;
+		num1 = 10;
+		num2 = 12424;
+//ch = "E"
+				isCompleted = false;
+		System.out.println(num1);
+		System.out.println(num2);
+		 //System.out.println(ch);
+		 System.out.println(isCompleted);
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ 
